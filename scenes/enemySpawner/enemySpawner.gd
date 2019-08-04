13e8218ea5966587c1deec502bbfbdf0d978bf8d
Guilherme R. Ribeiro <keybpr@gmail.com>
2019-08-04 12:27:36 -0300

    Changed some things about enemy spawn, SHOULD be better now

--- scenes/enemySpawner/enemySpawner.gd
+++ scenes/enemySpawner/enemySpawner.gd
@@ -4,7 +4,17 @@ var enemyRanged=preload("res://scenes/enemyRanged/enemyRanged.tscn")
 var enemyMorningStar=preload("res://scenes/enemyMorningStar/enemyMorningStar.tscn")
 func _ready():randomize()
 func _on_timer_timeout():
-	$timer.wait_time=rand_range(1.0,2.0)
-	var i=enemyMorningStar.instance()
+	var xx = rand_range(20,80) if randi()%2==0 else rand_range(240,300)
+	var yy = rand_range(50,250)
+	self.global_position=Vector2(xx,yy)
+	var numberOfEnemies=0
+	for node in get_parent().get_children():
+		if node.is_in_group("Enemy"):
+			numberOfEnemies+=1
+	$timer.wait_time=(8/5)*numberOfEnemies+rand_range(1.0,2.0)
+	var percentage=randf()
+	if percentage<0.8:var i=enemy.instance()
+	elif percentage<0.95:var i=enemyRanged.instance()
+	else:var i=enemyMorningStar.instance()
 	i.vectorTargetPosition=self.global_position
 	get_parent().add_child(i)
